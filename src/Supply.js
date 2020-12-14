fed8909759f2fa9a14fbd36c691e5bc47959404b
jperalta <jperalta.henri@gmail.com>
2020-12-13 21:48:30 -0500

    pagination, fixed header

--- src/Supply.js
+++ src/Supply.js
@@ -8,9 +8,52 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  TablePagination,
+  TableSortLabel,
+  Toolbar,
+  Typography,
+  Checkbox,
+  Tooltip,
+  FormControlLabel,
+  Switch,
+  DeleteIcon,
+  FilterListIcon,
   Paper
 } from '@material-ui/core';
 import { supplyData } from "./data";
+
+
+const columns = [
+  {
+    id: 'app_bundle',
+    label: 'App Bundle',
+    maxWidth: 300,
+    align: 'left',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'category',
+    label: 'Category',
+    maxWidth: 300,
+    align: 'left',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'interstitial',
+    label: 'Interstitial',
+    maxWidth: 300,
+    align: 'center',
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: 'premium',
+    label: 'Premium',
+    maxWidth: 300,
+    align: 'center',
+    format: (value) => value.toFixed(2),
+  },
+];
+
 
 
 const StyledTableCell = withStyles((theme) => ({
@@ -31,9 +74,13 @@ const StyledTableRow = withStyles((theme) => ({
   },
 }))(TableRow);
 
+
 const useStyles = makeStyles({
-  table: {
-    minWidth: 500,
+  root: {
+    width: '100%',
+  },
+  container: {
+    maxHeight: 440,
   },
 });
 
@@ -42,33 +89,63 @@ const useStyles = makeStyles({
 export const Supply = () => {
   const classes = useStyles();
 
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} size="small" aria-label="a dense table">
+  <Paper className={classes.root}>
+    <TableContainer className={classes.container}>
+      <Table stickyHeader aria-label="sticky table">
       <TableHead>
         <TableRow>
-          <StyledTableCell align="left">App Bundle</StyledTableCell>
-          <StyledTableCell align="left">Name</StyledTableCell>
-          <StyledTableCell>Category</StyledTableCell>
-          <StyledTableCell align="center">Interstitial</StyledTableCell>
-          <StyledTableCell align="center">Premium</StyledTableCell>
+          {columns.map((column) => (
+              <TableCell
+                key={column.id}
+                align={column.align}
+                style={{ minWidth: column.minWidth }}
+              >
+                {column.label}
+              </TableCell>
+            ))}
         </TableRow>
       </TableHead>
-        <TableBody>
-          {supplyData.map((data,key) => (
-            <StyledTableRow key={key}>
-              <StyledTableCell component="th" scope="row" width="350">
-                {data.app_bundle}
-              </StyledTableCell>
-              <StyledTableCell align="left" width="300">{data.name}</StyledTableCell>
-              <StyledTableCell align="left" width="150">{data.category}</StyledTableCell>
-              <StyledTableCell align="center">{data.interstitial}</StyledTableCell>
-              <StyledTableCell align="center">{data.premium}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
 
+      <TableBody>
+            {supplyData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+              return (
+                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  {columns.map((column) => {
+                    const value = row[column.id];
+                    return (
+                      <TableCell key={column.id} align={column.align}>
+                        {column.format && typeof value === 'number' ? column.format(value) : value}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
       </Table>
     </TableContainer>
+    <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={supplyData.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+  </Paper>
   );
 };
